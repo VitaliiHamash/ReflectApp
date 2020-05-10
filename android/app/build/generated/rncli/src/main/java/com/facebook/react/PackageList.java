d0fc64370f0ada06c91a693d888597b8de8bb710
VitaliiHamash <vitalii.m.hamash@gmail.com>
2020-05-10 09:40:28 -0700

    add Image Picker

--- android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
+++ android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
@@ -18,6 +18,8 @@ import com.navigation5.R;
 import org.reactnative.maskedview.RNCMaskedViewPackage;
 // react-native-gesture-handler
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+// react-native-image-picker
+import com.imagepicker.ImagePickerPackage;
 // react-native-reanimated
 import com.swmansion.reanimated.ReanimatedPackage;
 // react-native-safe-area-context
@@ -73,6 +75,7 @@ public class PackageList {
       new MainReactPackage(mConfig),
       new RNCMaskedViewPackage(),
       new RNGestureHandlerPackage(),
+      new ImagePickerPackage(),
       new ReanimatedPackage(),
       new SafeAreaContextPackage(),
       new RNScreensPackage(),
--- app/src/addPhoto.js
+++ app/src/addPhoto.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native';
+import { Text, View,Image, Button } from 'react-native';
+import ImagePicker from 'react-native-image-picker';
 
-
+import styles from './styles/styles'
 export default class AddPhoto extends Component {
+
+  state = {
+    photo: null
+  }
+  
+  handleChooseImage = () => {
+
+    const options = {
+      noData:true,
+    }; 
+    ImagePicker.launchImageLibrary(options, response => {
+      
+      if(response.uri){
+        this.setState({photo:response});
+        console.log(this.state)
+      }
+    })
+
+  }
+
+
   render() {
+    
     return (
-      <View>
-            <Text>Add Photo New</Text>
+      
+      <View style={{flex:1, alignItems:"center", justifyContent:"center"}}>
+            {this.state.photo && (
+              <Image
+                source={{uri:this.state.photo.uri}}
+                style={{width:300, height:300}}
+                />
+            )}
+
+            <Button
+                title="Choose Image"
+                onPress={this.handleChooseImage}
+            
+            />
       </View>
     )
   }
